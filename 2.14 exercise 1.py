eed286448f5d0ac3d4b0030099e40e10b43c2199
Triston Charles <tjcharles@mavs.coloradomesa.edu>
2025-02-27 15:34:06 -0700

    Add files via upload

---
+++ 2.14 exercise 1.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:     2.14 exercise 1
+#
+# Author:      tristonc
+#
+# Created:     02/06/2025
+# Copyright:   (c) johnk 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#All work and no play makes Jack a dull boy
+
+one = "All"
+two = "work"
+three = "and"
+four = "no"
+five = "play"
+six = "makes"
+seven = "Jack"
+eight = "a"
+nine = "dull"
+ten = "boy"
+
+print(one, two, three, four, five, six, seven, eight, nine, ten)
+
