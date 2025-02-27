a8d1f7a655bbc7bdcf9c7ed3a260e998b616008b
Triston Charles <tjcharles@mavs.coloradomesa.edu>
2025-02-27 15:34:29 -0700

    Add files via upload

---
+++ 3.8 exercise 11.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:     3.8 exercise 11
+#
+# Author:      tristonc
+#
+# Created:     02/14/2025
+# Copyright:   (c) johnk 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import turtle
+wn = turtle.Screen()
+tess = turtle.Turtle()
+
+tess.forward(100)
+tess.right(144)
+tess.forward(100)
+tess.right(144)
+tess.forward(100)
+tess.right(144)
+tess.forward(100)
+tess.right(144)
+tess.forward(100)
+
+wn.mainloop()
